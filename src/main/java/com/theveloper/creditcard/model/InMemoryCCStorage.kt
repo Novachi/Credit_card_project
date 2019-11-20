cd208adc3e61c85ad5482d74378f932ac118044b
Oskar Gawlak <oskargawlak@Oskars-MacBook-Pro.local>
2019-11-20 23:39:00 +0100

    Fixed tests

--- src/main/java/com/theveloper/creditcard/model/InMemoryCCStorage.kt
+++ src/main/java/com/theveloper/creditcard/model/InMemoryCCStorage.kt
@@ -1,11 +1,11 @@
 package com.theveloper.creditcard.model
 
-internal class InMemoryCCStorage {
+internal class InMemoryCCStorage(val storage: HashMap<String, CreditCard>) {
     fun add(card: CreditCard) {
-
+        storage[card.cardNumber] = card
     }
 
     fun load(creditCardNumber: String): CreditCard? {
-        return null
+        return storage[creditCardNumber]
     }
 }
